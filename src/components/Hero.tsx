24bfc1ec9feeb64e7e88078888fedc825fafa63c
Sebastian <mrseb10@gmail.com>
2024-05-01 16:15:55 +0200

    Hero: Name Glitch Effect

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,17 @@
+import { revalia } from "@/fonts";
+
 export default function Hero() {
-  return <section id="hero">Hero</section>;
+  return (
+    <section id="hero" className={revalia.className}>
+      <div className="flex h-full items-center justify-center">
+        <h1 className="relative">
+          Sebastian Opozda
+          <span className="absolute left-0 top-0">Sebastian Opozda</span>
+          <span className="absolute left-0 top-0 text-clip">
+            Sebastian Opozda
+          </span>
+        </h1>
+      </div>
+    </section>
+  );
 }
---
+++ src/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Revalia } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const revalia = Revalia({ weight: ["400"], subsets: ["latin"] });
