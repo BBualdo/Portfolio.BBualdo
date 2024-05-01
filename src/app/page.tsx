356746aa6167f6c225522d97c0e2e0fec031e4fa
Sebastian <mrseb10@gmail.com>
2024-05-01 13:16:46 +0200

    Sections templates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <main>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Contact />
+    </main>
+  );
 }
