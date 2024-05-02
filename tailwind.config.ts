0cfa01f162bcae39bdd86aecb433c21442c0c485
Sebastian <mrseb10@gmail.com>
2024-05-03 00:43:45 +0200

    Custom Scrollbar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,15 +15,9 @@ const config: Config = {
   },
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       colors: {
         vicePink: "rgb(255, 0, 157)",
         viceCyan: "rgb(0, 174, 255)",
-        astro: "FFF",
       },
     },
   },
