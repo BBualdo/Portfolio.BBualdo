a30957a707c9bd4a816be125b1b2af22758fcacf
Sebastian <mrseb10@gmail.com>
2024-05-02 19:00:50 +0200

    Project cards demo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,12 +1,66 @@
+import { projects } from "@/constants/projects";
+import Image from "next/image";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 export default function Projects() {
+  const projectsElement = projects.map((project, index) => {
+    const firstOrder = index % 2 == 0 ? 1 : 2;
+    const secondOrder = index % 2 == 0 ? 2 : 1;
+    // image desktop
+    return (
+      <div className="flex rounded-xl border-4">
+        <div className={`relative order-${firstOrder} flex-1`}>
+          <Image src={project.image.desktop} alt="" fill objectFit="contain" />
+        </div>
+        <div
+          className={`order-${secondOrder} flex flex-1 flex-col gap-10 px-10 py-6`}
+        >
+          <p className="text-2xl font-black uppercase text-viceCyan">
+            {project.name}
+          </p>
+          <p>{project.description}</p>
+          <div>
+            <p className="font-bold uppercase">Technologies:</p>
+            <ul>
+              {project.technologies.map((t) => (
+                <li className="my-2 mr-2 inline-block rounded-xl border-2 border-viceCyan bg-viceCyan/10 px-4 py-2 font-bold text-white shadow-[0_4px_0] shadow-viceCyan">
+                  {t}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="flex items-center gap-4">
+            <a
+              href={project.linkToRepo}
+              target="_blank"
+              className="flex items-center gap-2 border-2 border-white bg-black px-4 py-2 font-bold uppercase transition-all duration-200 hover:border-vicePink hover:text-vicePink"
+            >
+              Source Code <FaExternalLinkAlt />
+            </a>
+            {project.linkToLive ? (
+              <a
+                href={project.linkToLive}
+                target="_blank"
+                className="flex items-center gap-2 px-4 py-2 font-bold uppercase transition-all duration-200 hover:text-viceCyan"
+              >
+                Live Site <FaExternalLinkAlt />
+              </a>
+            ) : null}
+          </div>
+
+          <p className="font-bold text-green-500">
+            Finished: {project.finishDate}
+          </p>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <section id="projects">
       <div>
-        <h3>
-          Here you can browse my latest projects. These are result of my hard
-          work, everything I'll learn is translated into new project to practice
-          new skills.
-        </h3>
+        <h3></h3>
+        <div className="flex flex-col gap-20">{projectsElement}</div>
       </div>
     </section>
   );
--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -1,6 +1,7 @@
 export type ProjectType = {
   id: number;
   name: string;
+  description: string;
   technologies: string[];
   finishDate: string;
   image: { desktop: string; mobile: string };
@@ -13,7 +14,10 @@ export const projects: ProjectType[] = [
   {
     id: 1,
     name: "Life is a Game",
+    description:
+      "An application where users can set missions for themselves, track their progress, and earn XP and levels based on the mission's difficulty level. The app also features a built-in achievement system. Currently, user data is stored in the browser's localStorage, but there are plans to expand it with a database and authentication.",
     technologies: [
+      "UI/UX Design",
       "NextJS",
       "TypeScript",
       "TailwindCSS",
@@ -22,19 +26,22 @@ export const projects: ProjectType[] = [
       "Zod",
       "Redux Toolkit",
     ],
-    finishDate: "23-01-2024 (BETA)",
+    finishDate: "23-01-2024 (Beta)",
     image: {
       desktop: "/screenshots/liag/desktop.png",
       mobile: "/screenshots/liag/mobile.png",
     },
     linkToRepo: "https://github.com/BBualdo/Life-is-a-Game",
-    linkToLive: "life-is-a-game.vercel.app",
+    linkToLive: "https://life-is-a-game.vercel.app",
     slug: "/overview/liag",
   },
   {
     id: 2,
     name: "ClassCarsLLC",
+    description:
+      "A website created for a client specializing in importing cars from Dubai. The site includes several subpages with separate functionalities such as FAQ Accordion, Image Slider, Contact Form, etc.",
     technologies: [
+      "UI/UX Design",
       "NextJS",
       "TypeScript",
       "TailwindCSS",
@@ -53,7 +60,15 @@ export const projects: ProjectType[] = [
   {
     id: 3,
     name: "Salegency",
-    technologies: ["Astro", "TypeScript", "TailwindCSS", "Framer Motion"],
+    description:
+      "A simple landing page created for a client who runs a company that helps other businesses increase their revenue. Website has Cookiebot and Calendly extensions embedded.",
+    technologies: [
+      "UI/UX Design",
+      "Astro",
+      "TypeScript",
+      "TailwindCSS",
+      "Framer Motion",
+    ],
     finishDate: "06-03-2024",
     image: {
       desktop: "/screenshots/salegency/desktop.png",
