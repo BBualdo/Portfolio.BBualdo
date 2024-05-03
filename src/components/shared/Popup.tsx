60e606558a99f0689056f85cd620752a4cb96bb6
Sebastian <mrseb10@gmail.com>
2024-05-03 10:58:23 +0200

    Popup animation

--- src/components/shared/Popup.tsx
+++ src/components/shared/Popup.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { fadeIn } from "@/utils/fadeIn";
+
 const Popup = () => {
   return (
-    <div className="fixed top-2 w-full">
+    <motion.div
+      variants={fadeIn("up", 0, 1, 0.5)}
+      initial="hidden"
+      whileInView="show"
+      className="fixed top-4 w-full"
+    >
       <p className="mx-auto max-w-[300px] rounded-xl border-4 border-white bg-black px-6 py-2 text-center font-bold text-white">
         Email coppied to clipboard.
       </p>
-    </div>
+    </motion.div>
   );
 };
 
