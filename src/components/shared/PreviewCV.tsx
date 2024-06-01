1e7aa07da289f2cb43a69599f76f42ceee97829f
Sebastian <mrseb10@gmail.com>
2024-06-01 15:33:38 +0200

    Update CV

--- src/components/shared/PreviewCV.tsx
+++ src/components/shared/PreviewCV.tsx
@@ -1,7 +1,7 @@
-const DownloadCV = () => {
+const PreviewCV = () => {
   return (
     <a
-      href="https://drive.google.com/file/d/1NnCzb--nd5qlcZg2msYKnZyVT11tkVKI/view?usp=drive_link"
+      href="https://drive.google.com/file/d/11s5V7dJ2NaX8uuOnkgcvz-yKamNEQFEs/view?usp=drive_link"
       id="linkToCV"
       aria-label="Link to CV"
       className="pointer border-4 border-white/50 px-4 py-2 font-bold text-white/50 transition-all duration-200 hover:border-white hover:text-white hover:shadow-white"
@@ -11,4 +11,4 @@ const DownloadCV = () => {
   );
 };
 
-export default DownloadCV;
+export default PreviewCV;
